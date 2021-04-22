a45d932fc478a4b266121909a04704e337983808
Jeffrey M. Thompson <jmthompson2015@users.noreply.github.com>
2021-04-22 10:52:59 -0600

    Added tests for UrlGenerator.

--- src/model/TestSuite.js
+++ src/model/TestSuite.js
@@ -1,14 +1,8 @@
 import Logger from "../utility/Logger.js";
 
-// import ActionTest from "./Action.test.js";
-// import BookTest from "./Book.test.js";
-// import BookComparatorTest from "./BookComparator.test.js";
 import MysteryBookLoaderTest from "./MysteryBookLoader.test.js";
-// import NominationTest from "./Nomination.test.js";
-// import NominationComparatorTest from "./NominationComparator.test.js";
-// import ReducerTest from "./Reducer.test.js";
 import SciFiBookLoaderTest from "./SciFiBookLoader.test.js";
-// import UserSettingsTest from "./UserSettings.test.js";
+import UrlGeneratorTest from "./UrlGenerator.test.js";
 
 window.LOGGER = new Logger();
 LOGGER.setTraceEnabled(false);
---
+++ src/model/UrlGenerator.test.js
@@ -0,0 +1,80 @@
+import Library from "../artifact/Library.js";
+import MysteryAward from "../artifact/MysteryAward.js";
+
+import UrlGenerator from "./UrlGenerator.js";
+
+QUnit.module("UrlGenerator");
+
+QUnit.test("createAmazonSearchUrl()", (assert) => {
+  // Setup.
+  const subject = "something";
+
+  // Run.
+  const result = UrlGenerator.createAmazonSearchUrl(subject);
+
+  // Verify.
+  assert.ok(result);
+  assert.equal(
+    result,
+    "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Dstripbooks&field-keywords=something"
+  );
+});
+
+QUnit.test("createAwardUrl()", (assert) => {
+  // Setup.
+  const award = MysteryAward.properties[MysteryAward.AGATHA];
+  const year = 2021;
+
+  // Run.
+  const result = UrlGenerator.createAwardUrl(award, year);
+
+  // Verify.
+  assert.ok(result);
+  assert.equal(
+    result,
+    "http://stopyourekillingme.com/Awards/Agatha_Awards.html"
+  );
+});
+
+QUnit.test("createGoodreadsSearchUrl()", (assert) => {
+  // Setup.
+  const subject = "something";
+
+  // Run.
+  const result = UrlGenerator.createGoodreadsSearchUrl(subject);
+
+  // Verify.
+  assert.ok(result);
+  assert.equal(result, "https://www.goodreads.com/search?q=something");
+});
+
+QUnit.test("createLibrarySearchUrl()", (assert) => {
+  // Setup.
+  const library = Library.properties[Library.DCL];
+  const subject = "something";
+
+  // Run.
+  const result = UrlGenerator.createLibrarySearchUrl(library, subject);
+
+  // Verify.
+  assert.ok(result);
+  assert.equal(
+    result,
+    "http://dcl.bibliocommons.com/search?q=something&t=smart&search_category=keyword"
+  );
+});
+
+QUnit.test("createWikipediaSearchUrl()", (assert) => {
+  // Setup.
+  const subject = "something";
+
+  // Run.
+  const result = UrlGenerator.createWikipediaSearchUrl(subject);
+
+  // Verify.
+  assert.ok(result);
+  assert.equal(result, "https://en.wikipedia.org/wiki/something");
+});
+
+const UrlGeneratorTest = {};
+export default UrlGeneratorTest;
